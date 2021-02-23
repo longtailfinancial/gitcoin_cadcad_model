d57faadfa0463a55cad42acc8c28beb5fe3dc899
Shawn Anderson <shawnltf@pm.me>
2021-02-22 21:07:15 -0800

    Make the optimizer test run.

--- optimality_gap/test_optimizer.py
+++ optimality_gap/test_optimizer.py
@@ -1,7 +1,7 @@
 import networkx as nx
 import random
 from typing import Callable, List
-from subgraph_optimizer import *
+from optimality_gap.subgraph_optimizer import optimize_subgraph_connectivity
 
 def create_test_graph(n: int, seed: int) -> nx.Graph:
     internet: nx.Graph = nx.random_internet_as_graph(n, seed=seed)
@@ -17,11 +17,20 @@ def utility_function(G): return nx.wiener_index(G)
 
 def test_utility():
     test_graph: nx.Graph = create_test_graph(50, 42)
-    result: tuple = optimize_graph_connectivity(test_graph, utility_function)
+    test_nodes = set(test_graph.nodes)
+    result: tuple = optimize_subgraph_connectivity(graph=test_graph,
+                                                   nodes=test_nodes,
+                                                   utility_function=utility_function)
     print(f"Best subgraph: {result[0]}")
     print(f"Best score: {result[1]}")
 
 
 if __name__ == '__main__':
     test_utility()
+
+# def optimize_subgraph_connectivity(graph: nx.Graph,
+                                   # nodes: set,
+                                   # utility_function: Callable[[nx.Graph], float],
+                                   # n_iter: int = 50,
+                                   # **kwargs) -> Tuple[nx.Graph, float]:
 
